f8c0a32ef222e53bcbb4749a5516b72f18962217
mmansion <mikhail@transmission.studio>
2023-08-20 11:26:11 -0400

    cpanel module

--- simulator/main.js
+++ simulator/main.js
@@ -1,19 +1,57 @@
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
-let toggleOrtho = true;
+//import control panel from public folder
+import ControlPanel from './public/ControlPanel.js';
+
+
+console.log(ControlPanel);
+
+const scene = new THREE.Scene();
+
+
+
+//add an arrow helper for the x, y, z axis
+const axesHelper = new THREE.AxesHelper(100);
+scene.add(axesHelper);
+
+function addGrid() {
+    const size = 1000;
+    const divisions = 10;
+ 
+    const gridHelper = new THREE.GridHelper(size, divisions);
+
+    //change color of grid lines
+    // gridHelper.material.color = new THREE.Color(0x0000ff);
+
+    //change material of grid lines
+    gridHelper.material = new THREE.LineBasicMaterial({
+       // make a transparent line color
+        color: 0x0000ff,
+        opacity: 0.2,
+        transparent: true,
+        //make thin lines
+        linewidth: 1
+    });
+
+
+
+    scene.add(gridHelper);
+}
+addGrid();
+
+
+let toggleOrtho = false;
 
 const frustumSize = 500;
-const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 
 const aspect = window.innerWidth / window.innerHeight;
 
-// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-let camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+// let camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -26,14 +64,21 @@ const cube = new THREE.Mesh(geometry, material);
 // scene.add(cube);
 
 
-
 //plane geometry
 const planeGeom = new THREE.PlaneGeometry(100, 100);
 // const material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
 const plane = new THREE.Mesh(planeGeom, material);
 scene.add(plane);
 
-camera.position.set(0, 0, 1000);
+let controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", event => {
+    console.log(controls.object.position);
+});
+controls.update();
+
+camera.position.set(598, 598, 598);
+camera.zoom = 4;
+camera.lookAt(0, 0, 0);
 
 function addSphere() {
     const geometry = new THREE.SphereGeometry(15, 32, 16);
@@ -49,154 +94,14 @@ function animate() {
     cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
-createPanel();
+
+let cpanel = new ControlPanel(camera);
+
+//last step
 animate();
 
-function createPanel() {
-
-    const panel = new GUI();
-    const f1 = panel.addFolder('camera').open();
 
 
-    let settings = {
-        zoom: () => {
-            camera.position.set(0, 0, 1000);
-            camera.up.set(0, 0, -1);
-            camera.updateProjectionMatrix();
-        },
-        handleClick: function () {
-            if (toggleOrtho) {
-                camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-                camera.position.z = 500;
-                toggleOrtho = false;
-            } else {
-                camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 1000);
-                camera.position.z = 100;
-                toggleOrtho = true;
-            }
-        }
-    };
-
-    // folder1.add(settings, 'handleClick').name('toggle camera');
-    f1.add(camera.position, 'z', 1, 2).onChange( (value) => {
-        // make camera look down
-        camera.position.set(0, 0, 1000);
-        camera.zoom = value;
-        console.log(value);
-    });
-    // camera.position.z = 100;
-    // let zoomControl = f1.add(settings, 'zoom').name('zoom');
-    // zoomControl.onChange(function (value) {
-    //     console.log(value);
-    // });
-
-    //add a button
-    f1.add(settings, 'handleClick').name('toggle camera');
-
- 
-}
-
-// let scene2, renderer2;
-
-// const frustumSize = 500;
-
-// init();
-// animate();
-
-// function init() {
-    
-//     const scene = new THREE.Scene();
-   
-//     const aspect = window.innerWidth / window.innerHeight;
-    // camera = new THREE.OrthographicCamera(frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 1000);
-    // scene.background = new THREE.Color(0xf0f0f0);
-
-
-    // // make the camera look down
-    // camera.position.set(0, 10, 0);
-    // camera.up.set(0, 0, -1);
-
-    // //The lookAt() method calls are necessary so the camera looks at the coordinates systems origin 
-    // //or at the position of the scene (which is more or less the root object of the scene graph). 
-    // //In most cases, the position value of scene is (0,0,0) (the origin).
-    // camera.lookAt(0, 0, 0);
-    
-
-
-    // scene2 = new THREE.Scene();
-
-    // const material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true, wireframeLinewidth: 40, side: THREE.DoubleSide });
-
-    // const geometry = new THREE.BoxGeometry(100, 100, 100);
-    // // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // const cube = new THREE.Mesh(geometry, material);
-    // scene.add(cube);
-
-    // left
-    // createPlane(
-    //     100, 100,
-    //     'chocolate',
-    //     new THREE.Vector3(- 50, 0, 0),
-    //     new THREE.Euler(0, - 90 * THREE.MathUtils.DEG2RAD, 0)
-    // );
-    // // right
-    // createPlane(
-    //     100, 100,
-    //     'saddlebrown',
-    //     new THREE.Vector3(0, 0, 50),
-    //     new THREE.Euler(0, 0, 0)
-    // );
-    // // top
-    // createPlane(
-    //     100, 100,
-    //     'yellowgreen',
-    //     new THREE.Vector3(0, 50, 0),
-    //     new THREE.Euler(- 90 * THREE.MathUtils.DEG2RAD, 0, 0)
-//     // );
-//     // bottom
-//     createPlane(
-//         300, 300,
-//         'blue',
-//         new THREE.Vector3(0, - 50, 0),
-//         new THREE.Euler(- 90 * THREE.MathUtils.DEG2RAD, 0, 0)
-//     );
-
-//     renderer = new THREE.WebGLRenderer();
-//     renderer.setPixelRatio(window.devicePixelRatio);
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     document.body.appendChild(renderer.domElement);
-
-//     // renderer2 = new CSS3DRenderer();
-//     // renderer2.setSize(window.innerWidth, window.innerHeight);
-//     // renderer2.domElement.style.position = 'absolute';
-//     // renderer2.domElement.style.top = 0;
-//     // document.body.appendChild(renderer2.domElement);
-
-//     const controls = new OrbitControls(camera, renderer.domElement);
-//     controls.minZoom = 0.5;
-//     controls.maxZoom = 2;
-
-//     function createPlane(width, height, cssColor, pos, rot) {
-
-//         const element = document.createElement('div');
-//         element.style.width = width + 'px';
-//         element.style.height = height + 'px';
-//         element.style.opacity = 0.75;
-//         element.style.background = cssColor;
-
-//         const object = new CSS3DObject(element);
-//         object.position.copy(pos);
-//         object.rotation.copy(rot);
-//         // scene2.add(object);
-
-//         const geometry = new THREE.PlaneGeometry(width, height);
-//         const mesh = new THREE.Mesh(geometry, material);
-//         mesh.position.copy(object.position);
-//         mesh.rotation.copy(object.rotation);
-//         scene.add(mesh);
-
-//     }
 
 //     window.addEventListener('resize', onWindowResize);
 //     createPanel();
---
+++ simulator/public/ControlPanel.js
@@ -0,0 +1,13 @@
+//import gui from threejs
+// import * as dat from 'dat.gui';
+import {GUI} from 'dat.gui';
+
+export default class ControlPanel {
+    constructor(camera) {
+    
+        this._gui = new GUI();
+        this._cameraSettings = this._gui.addFolder('Camera');
+        this._cameraSettings.add(camera.position, 'x', -100, 100).listen();
+   }
+
+}
